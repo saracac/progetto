4a12d7fbae76ae28646986528ed752bb1a48f0d5
carlo <Betacom@DESKTOP-1SDDRN1>
2022-03-18 09:40:03 +0100

    Junit di amministratoreBC e quindi del dao dello stesso, ancora da testare poichè non ci sono dati sul db e non c'è il dbaccess

---
+++ project/src/main/java/test/com/project/businesscomponent/AmministratoreBCTest.java
@@ -0,0 +1,54 @@
+package test.com.project.businesscomponent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.project.architecture.dao.AmministratoreDAO;
+import com.project.businesscomponent.model.Amministratore;
+
+class AmministratoreBCTest {
+	Connection conn;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		conn=DBAccess.getConnection();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		conn=DBAccess.close();
+	}
+
+	@Test
+	void testGetByPk() {
+		try {
+			
+			//Test non ancora effettuato il codAdmin dovra essere cambiato una volta inseriti dei dati di prova nel db
+			long codAdmin=00L;
+			Amministratore admin=new Amministratore();
+			admin=AmministratoreDAO.getFactory().getByPk(conn, codAdmin);
+			System.out.println(admin.toString());
+			
+		}catch(SQLException exc) {
+			exc.printStackTrace();
+			System.out.println(exc.getMessage());
+			System.out.println(exc.getCause());
+			System.out.println(exc.getErrorCode());
+			fail();
+			
+		}catch(Exception exc) {
+			exc.printStackTrace();
+			System.out.println(exc.getMessage());
+			fail();
+		}
+	}
+
+}
