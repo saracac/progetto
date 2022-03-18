0472c6c3a453d9bfcca393c5f7b27682eb402a92
Betacom <Betacom@DESKTOP-1SDDRN1>
2022-03-18 11:14:55 +0100

    modifica DAOConstants

--- project/src/main/java/com/project/architecture/dao/DAOConstants.java
+++ project/src/main/java/com/project/architecture/dao/DAOConstants.java
@@ -1,6 +1,15 @@
 package com.project.architecture.dao;
 
 public interface DAOConstants {
+	
+	// AMMINISTRATORE
 	String SELECT_ADMIN_BYPK="Select nomeAdmin,cognomeAdmin from Amministratore where codAdmin=?"; 
-
+	
+	//DOCENTE
+	
+	//CORSO
+	
+	//CORSISTA
+	
+	
 }
