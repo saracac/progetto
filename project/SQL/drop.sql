5fcd644a114eee01a54828c9b2280b1215a694e9
Eugenio <roverato.eugenio@gmail.com>
2022-03-21 16:15:24 +0100

    Add SQL: create, insert, drop

---
+++ project/SQL/drop.sql
@@ -0,0 +1,14 @@
+-- CORSISTA
+drop table corsista cascade constraints;
+
+-- ADMIN
+drop table admin cascade constraints;
+
+-- CORSO
+drop table corso cascade constraints;
+
+-- DOCENTE
+drop table docente cascade constraints;
+
+-- CORSO_CORSISTA
+drop table corsocorsista cascade constraints;
